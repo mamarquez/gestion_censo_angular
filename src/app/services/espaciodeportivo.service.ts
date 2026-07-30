1ce3cfaa16d0aeb62d13bd4ddd68713c7b9998dc
Miguel A. Márquez Vargas <duncani670@gmail.com>
2026-07-30 20:23:10 +0200

    Fin 1º fase de filtros

--- src/app/services/espaciodeportivo.service.ts
+++ src/app/services/espaciodeportivo.service.ts
@@ -1,10 +1,13 @@
 import { Injectable, inject } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
 
 import { EspacioDeportivo } from '../models/espaciodeportivo';
 import { AUTH } from '../auth/auth.constants';
 import { ApiResponse } from '../models/apiresponse';
+import { NivelEducativo } from '../models/niveleducativo';
+import { ApiResponseWrapper } from '../interface/api-response-wrapper.interface';
+import { buildHttpParams } from '../utils/params.util';
 
 
 @Injectable({
@@ -14,12 +17,33 @@ export class EspacioDeportivoService {
 
   private readonly http = inject(HttpClient);
   private readonly api = `${AUTH.API}/espaciosdeportivos`;
+  private readonly headers = new HttpHeaders({
+    'Content-Type': 'application/json'
+  });
 
-
-   /**
-   * Obtener todos los espacios deportivos
+  /**
+   * Obtener todos los niveles educativos
    */
-  getAll(): Observable<ApiResponse<EspacioDeportivo[]>> {
-    return this.http.get<ApiResponse<EspacioDeportivo[]>>(this.api);
+  getAll(filtros?: any): Observable<ApiResponse<EspacioDeportivo[]>> {
+    return this.http.get<ApiResponseWrapper<EspacioDeportivo[]>>(`${this.api}`, {
+      params: buildHttpParams(filtros),
+      headers: this.headers
+    });
+  }
+
+  /**
+   * Cambia el estado de un registro
+   * @param id Id del registro
+   */
+  cambiarEstado(id: number): Observable<ApiResponse<EspacioDeportivo>> {
+    return this.http.patch<ApiResponse<EspacioDeportivo>>(`${this.api}/${id}`, null, { headers: this.headers });
+  }
+
+  /**
+   * Borra un registro
+   * @param id Id del registro
+   */
+  borrarRegistro(id: number): Observable<ApiResponse<EspacioDeportivo>> {
+    return this.http.delete<ApiResponse<EspacioDeportivo>>(`${this.api}/${id}`, { headers: this.headers });
   }
 }
--- src/app/views/espaciosdeportivos/espaciodeportivo.component.ts
+++ src/app/views/espaciosdeportivos/espaciodeportivo.component.ts
@@ -4,23 +4,75 @@ import { TableModule } from 'primeng/table';
 
 import { EspacioDeportivo } from '../../models/espaciodeportivo';
 import { EspacioDeportivoService } from '../../services/espaciodeportivo.service';
+import { NivelEducativo } from '../../models/niveleducativo';
+import { FormBuilder, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { Button } from 'primeng/button';
+import { InputText } from 'primeng/inputtext';
+import { ConfirmDialogModule } from 'primeng/confirmdialog';
+import { TooltipModule } from 'primeng/tooltip';
+import { MessageService } from 'primeng/api';
+import { DialogService } from '../../services/dialog.service';
 
 @Component({
+  standalone: true,
   selector: 'app-espacio-deportivo',
-  imports: [TableModule],
+  imports: [TableModule, Button, InputText, ReactiveFormsModule, ConfirmDialogModule, TooltipModule],
   templateUrl: './espaciodeportivo.component.html',
   styleUrl: './espaciodeportivo.component.css',
 })
 export class EspacioDeportivoCompoment implements OnInit {
 
+  private readonly fb = inject(FormBuilder);
   private readonly service = inject(EspacioDeportivoService);
   private readonly cdr = inject(ChangeDetectorRef);
+  private readonly messageService = inject(MessageService);
+  private readonly dialog = inject(DialogService);
 
   espaciosDeportivos: EspacioDeportivo [] = [];
   cargando: boolean = true;
 
+  form: FormGroup = this.fb.group({
+    id: [''],
+    nombre: [''],
+    descripcion: [''],
+    activo: ['']
+  });
+
   ngOnInit(): void {
     this.cargar();
+  }
+
+  limpiar(): void {
+    this.form.reset();
+    this.buscar();
+  }
+
+  buscar(): void {
+    const filtros = this.form.value;
+    this.cargando = true;
+
+    this.service.getAll(filtros).subscribe({
+      next: (response) => {
+        if (response && Array.isArray(response.data)) {
+          this.espaciosDeportivos = response.data;
+        } else {
+          this.espaciosDeportivos = [];
+        }
+
+        this.cargando = false;
+        this.cdr.markForCheck();
+      },
+      error: (err) => {
+        console.error('Error cargando provincias:', err);
+        this.messageService.add({
+          severity: 'error',
+          summary: 'Error',
+          detail: 'Error al cargar los gestores'
+        });
+        this.cargando = false;
+        this.espaciosDeportivos = [];
+      }
+    });
   }
 
   cargar(): void {
@@ -31,11 +83,72 @@ export class EspacioDeportivoCompoment implements OnInit {
         this.cdr.detectChanges();
       },
       error: (err) => {
-        console.error('Error al cargar gestores', err);
+        console.error('Error al cargar espacios deportivos', err);
         this.cargando = false;
         this.cdr.detectChanges();
       }
     });
+  }
+
+  /**
+   * Cambia el estado de un registro
+   * @param id Id del registro
+   */
+  cambiarEstado(id: number): void {
+    this.cargando = true;
+
+    this.service.cambiarEstado(id).subscribe({
+      next: () => {
+        const espacioDeportivo = this.espaciosDeportivos.find(p => p.id === id);
+        if (espacioDeportivo) {
+          espacioDeportivo.activo = !espacioDeportivo.activo;
+        }
+
+        this.messageService.add({ severity: 'success', summary: 'Actualizado', detail: 'Se ha actualizado el estado' });
+        this.cargando = false;
+        this.cdr.detectChanges();
+      },
+      error: (err) => {
+        console.error('Error al cambiar el estado de espacio deportivo', err);
+        this.messageService.add({ severity: 'error', summary: 'Error', detail: 'Error al actualizar el estado' });
+        this.cargando = false;
+        this.cdr.detectChanges();
+      }
+    });
+  }
+
+  confirmarBorrado(registro: EspacioDeportivo): void {
+    this.dialog.confirmar({
+      mensaje: `¿Deseas eliminar "<strong>${registro.nombre}"</strong>?`,
+      titulo: 'Confirmar eliminación',
+      labelAceptar: 'Sí, eliminar',
+      onAccept: () => this.borrarRegistro(registro.id)
+    });
+  }
+
+  private borrarRegistro(id: number) {
+    this.cargando = true;
+
+    this.service.borrarRegistro(id).subscribe({
+      next: () => {
+        this.espaciosDeportivos = this.espaciosDeportivos.filter(p => p.id !== id);
+        this.messageService.add({
+          severity: 'success',
+          summary: 'Eliminado',
+          detail: 'Se ha borrado el registro correctamente'
+        });
+        this.cargando = false;
+        this.cdr.detectChanges();
+      },
+      error: (err) => {
+        console.error('Error al borrar el registro', err);
+        this.messageService.add({ severity: 'error', summary: 'Error', detail: 'Error al borrar el registro' });
+        this.cargando = false;
+        this.cdr.detectChanges();
+      }
+    });
+
+    this.cargando = false;
   }
 
 }
