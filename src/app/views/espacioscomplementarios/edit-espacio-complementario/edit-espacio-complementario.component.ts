cb517b345f3646e7480af5d2ee4ec16d0f3fec81
Miguel A. Márquez Vargas <duncani670@gmail.com>
2026-08-18 11:16:46 +0200

    Fix iconos

--- src/app/views/espacioscomplementarios/edit-espacio-complementario/edit-espacio-complementario.component.ts
+++ src/app/views/espacioscomplementarios/edit-espacio-complementario/edit-espacio-complementario.component.ts
@@ -1,17 +1,36 @@
 import { ChangeDetectorRef, Component, DestroyRef, inject } from '@angular/core';
 import { InputText } from 'primeng/inputtext';
-import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import {
+  FormBuilder,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators
+} from '@angular/forms';
+
 import { EspacioComplementarioService } from '../../../services/espaciocomplementario.service';
 import { MessageService } from 'primeng/api';
 import { DialogService } from '../../../services/dialog.service';
 import { EspacioComplementario } from '../../../models/espaciocomplementario';
-import { TableModule } from 'primeng/table';
+
 import { Button } from 'primeng/button';
-import { ConfirmDialogModule } from 'primeng/confirmdialog';
-import { TooltipModule } from 'primeng/tooltip';
-import { AccionesTablaComponent } from '../../../utils/acciones-tabla/acciones-tabla.component';
-import { EditModalComponent } from '../../../components/modal/edit-modal/edit-modal.component';
-import { Truncar } from '../../../pipe/trucar.pipe';
+import { Fieldset } from 'primeng/fieldset';
+
+import { CKEditorModule } from '@ckeditor/ckeditor5-angular';
+
+import {
+  ClassicEditor,
+  Essentials,
+  Paragraph,
+  Bold,
+  Italic,
+  Heading,
+  Link,
+  List,
+  Undo
+} from 'ckeditor5';
+
+import coreTranslations from 'ckeditor5/translations/es.js';
+import 'ckeditor5/ckeditor5.css';
 
 /**
  * @version 1.0.0
@@ -21,9 +40,11 @@ import { Truncar } from '../../../pipe/trucar.pipe';
   standalone: true,
   selector: 'app-edit-espacio-complementario',
   imports: [
-    InputText,
     ReactiveFormsModule,
-    Button
+    InputText,
+    Button,
+    Fieldset,
+    CKEditorModule
   ],
   templateUrl: './edit-espacio-complementario.component.html'
 })
@@ -33,16 +54,50 @@ export class EditEspacioComplementarioComponent {
   private readonly service = inject(EspacioComplementarioService);
   private readonly cdr = inject(ChangeDetectorRef);
   private readonly messageService = inject(MessageService);
-  private readonly dialog = inject(DialogService);
   private readonly destroyRef = inject(DestroyRef);
 
+  public Editor = ClassicEditor;
+
+  public config = {
+    licenseKey: 'GPL',
+    language: 'es',
+    translations: [
+      coreTranslations
+    ],
+    plugins: [
+      Essentials,
+      Paragraph,
+      Bold,
+      Italic,
+      Heading,
+      Link,
+      List,
+      Undo
+    ],
+
+    toolbar: [
+      'undo',
+      'redo',
+      '|',
+      'heading',
+      '|',
+      'bold',
+      'italic',
+      '|',
+      'link',
+      '|',
+      'bulletedList',
+      'numberedList'
+    ]
+  };
+
   cargando = false;
+  guardando = false;
 
   form: FormGroup = this.fb.group({
     id: [null],
-    nombre: ['', [Validators.required, Validators.maxLength(EspacioComplementario.campos.nombre.maxLength) ]],
+    nombre: ['', [Validators.required, Validators.maxLength(EspacioComplementario.campos.nombre.maxLength)]],
     descripcion: [null],
     activo: [true]
   });
-
 }
